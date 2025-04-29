5b152f81bd4f9b8fe56deeb4ad449fae8f4dbc41
anais <mouelhi.chacha@gmail.com>
2025-04-29 16:55:11 +0200

    [ADD] test automatisation (CI/CD)

---
+++ tests/test_operations.py
@@ -0,0 +1,33 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Ajouter le répertoire racine au chemin Python
+
+from fastapi.testclient import TestClient
+from app import app  # Importing the FastAPI app object directly
+
+client = TestClient(app)
+
+def test_add():
+    response = client.get("/add?a=5&b=3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 8
+
+def test_subtract():
+    response = client.get("/subtract?a=5&b=3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 2
+
+def test_multiply():
+    response = client.get("/multiply?a=5&b=3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 15
+
+def test_divide():
+    response = client.get("/divide?a=6&b=3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 2
+
+def test_divide_by_zero():
+    response = client.get("/divide?a=5&b=0")
+    assert response.status_code == 200
+    assert response.json()["error"] == "Cannot divide by zero."
